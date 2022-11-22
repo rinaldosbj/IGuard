ba7b22e5cf839802d3b61648268080f7b142716f
miggelucas <lucasmigge@gmail.com>
2022-11-22 16:11:52 -0300

    first commit

---
+++ StayingAlive Watch App/StayingAliveApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayingAliveApp.swift
+//  StayingAlive Watch App
+//
+//  Created by Lucas Migge de Barros on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct StayingAlive_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
