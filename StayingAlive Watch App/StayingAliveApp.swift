84dfb01e819ad94c427e31db1fe5d514b43689fc
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2022-11-29 16:28:11 -0300

    first commit

--- StayingAlive Watch App/StayingAliveApp.swift
+++ StayingAlive Watch App/StayingAliveApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StayingAlive_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Onboarding3View()
         }
     }
 }
