cab83489052a1929aae2b09edcb40fdc1926f495
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2022-12-01 16:38:57 -0300

    fix(ChangingColors): new color set and refact in colors

---
+++ StayingAlive Watch App/Models/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StayingAlive Watch App
+//
+//  Created by Lucas Migge de Barros on 01/12/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let TurquoiseColor = "Turquoise"
+}
