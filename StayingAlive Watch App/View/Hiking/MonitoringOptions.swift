e5a10b3a967cec2125fed8c3da389eeff48fac10
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2022-12-05 23:19:14 -0300

    feature(MonitoringOptionsView): navigation for MonitoringOptionsView

--- StayingAlive Watch App/View/Hiking/MonitoringOptions.swift
+++ StayingAlive Watch App/View/Hiking/MonitoringOptions.swift
@@ -14,23 +14,28 @@ struct MonitoringOptions: View {
     let activities : [Activity] = Populate.getActivities()
     
     var body: some View {
+        
         List(activities, id: \.self) { activity in
-            Button(action: {
-                hikingManager.selectedWorkout = activity.workoutType
-            }) {
-                HStack{
-                    Text(activity.name.rawValue)
-                        .font(.headline)
-                        .foregroundColor(.accentColor)
-                    Spacer()
-                    activity.image
-                        .resizable()
-                        .frame(width: 25, height: 20)
-                        .padding(.trailing)
-                        .foregroundColor(.accentColor)
+            NavigationLink(destination: MonitandoView()) {
+                Button(action: {
+                    hikingManager.selectedWorkout = activity.workoutType
+                }) {
+                    HStack{
+                        Text(activity.name.rawValue)
+                            .font(.headline)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        activity.image
+                            .resizable()
+                            .frame(width: 25, height: 20)
+                            .padding(.trailing)
+                            .foregroundColor(.accentColor)
+
+                    }
                 }
             }
             .navigationTitle("Atividade")
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
